ebbe6b0b3b60c7d504add1d363f950c55ee64d0d
DaiDe4 <ivarvarin9@gmail.com>
2023-11-30 18:35:31 +0300

    commit 29 rdy

--- src/z29/z29.java
+++ src/z29/z29.java
@@ -5,16 +5,28 @@ import java.util.Scanner;
 public class z29 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int N = 5;
+        int[][] roads = new int[N][N];
+        for (int k = 0; k < N; k++) {
+            for (int y = 0; y < N; y++) {
+                roads[k][y] = 0;
+            }
+        }
+        roads[0][1] = 1;
+        roads[1][0] = 1;
+        roads[2][0] = 1;
+        roads[0][2] = 1;
+        roads[3][1] = 1;
+        roads[1][3] = 1;
         int sum = 0;
         int temp;
 
-        for (int i = 0; i < n * n; i++) {
-            temp = scanner.nextInt();
-            sum += temp;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                temp = roads[i][j];
+                sum += temp;
+            }
         }
-        scanner.close();
         System.out.println(sum / 2);
     }
 }
